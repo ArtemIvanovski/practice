69381812cac8701857787ec5dea1f7f6b84d865a
Artem <100025682+ArtemIvanovski@users.noreply.github.com>
2024-06-19 20:07:09 +0300

    Update request.py

--- course/http/request.py
+++ course/http/request.py
@@ -12,20 +12,20 @@ def fetch_status(url):
 if __name__ == "__main__":
     # 200
     status, data = fetch_status(
-        "https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=142b10f69c31d5d2568bf55ac959aca3")
+        "https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=4d29fffff502452bcd37d55f83b66401")
     print(f"Status: {status}, Data: {data}")
 
     # 400
     status, data = fetch_status(
-        "https://api.openweathermap.org/data/2.5/weather?lat=-&lon=10.99&appid=142b10f69c31d5d2568bf55ac959aca3")
+        "https://api.openweathermap.org/data/2.5/weather?lat=-&lon=10.99&appid=4d29fffff502452bcd37d55f83b66401")
     print(f"Status: {status}, Data: {data}")
 
     # 401
     status, data = fetch_status(
-        "https://api.openweathermap.org/data/3.0/onecall/timemachine?appid=627a64002a11e33bbea79bcb3c7ab7a2")
+        "https://api.openweathermap.org/data/3.0/onecall/timemachine?appid=aab2ed3489b0183f0de077dfe3e56826")
     print(f"Status: {status}, Data: {data}")
 
     # 404
     status, data = fetch_status(
-        "https://api.openweathermap.org/data/2.5/weathe?lat=-&lon=10.99&appid=142b10f69c31d5d2568bf55ac959aca3")
+        "https://api.openweathermap.org/data/2.5/weathe?lat=-&lon=10.99&appid=4d29fffff502452bcd37d55f83b66401")
     print(f"Status: {status}, Data: {data}")
